c188d4066be629f025bac962bc93f73613e864c7
Venkatachalam Mahadevan <vignesh14.vijay@gmail.com>
2023-08-30 13:23:42 +1200

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as sm
 
-sm.title('This is our new webpage using snowflake - the journey of two analysts becoming data engineers')
+sm.title('This is our new webpage using snowflake - the journey of two analysts becoming data engineers and a nice cook')
 
 sm.header('How to become a data engineer')
 sm.text('Finish snowflake badges')
 sm.text('Data Factory')
 sm.text('Databricks')
 sm.text('Finish Certifications of the above')
+
+#Breakfast cooking time
+sm.header('This is cooking session - Time to cook!')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔🍞 Hard-Boiled Free-Range Egg')
+          
