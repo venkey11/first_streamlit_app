fbab8387875ea2384e9d9ed12b82debb1f35c0a1
Venkatachalam Mahadevan <vignesh14.vijay@gmail.com>
2023-08-30 13:15:41 +1200

    Update streamlit_app.py
    steps in becoming data engineer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as sm
 
 sm.title('This is our new webpage using snowflake - the journey of two analysts becoming data engineers')
+
+sm.header('How to become a data engineer')
+sm.text('Finish snowflake badges')
+sm.text('Data Factory')
+sm.text('Databricks')
+sm.footer('Finish Certifications of the above')
